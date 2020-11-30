969bb2c0c7fc02ad92c88a823baa006ed022e3f8
DayanaM <dmmartinez269@misena.edu.co>
2020-11-30 10:39:47 -0500

    Updates

---
+++ precache-manifest.a153534b220cdf6d8ddbec57fdcc282c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68712e48edbb57c92ee2fe8a6a58c003",
+    "url": "/paosarmi/BOG001-burger-queen.git/index.html"
+  },
+  {
+    "revision": "e4d2cbebdd8f7c3aea4f",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/css/main.842c8413.chunk.css"
+  },
+  {
+    "revision": "2d9714e4094f8a64f79b",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/js/2.115edd51.chunk.js"
+  },
+  {
+    "revision": "70c1250a949326ab701ae38c8bc3a317",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/js/2.115edd51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d2cbebdd8f7c3aea4f",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/js/main.8ac66690.chunk.js"
+  },
+  {
+    "revision": "e274321a6aa6602f05ea",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/js/runtime-main.720c54d8.js"
+  },
+  {
+    "revision": "3f3eccd361c674e1cdff41193df35888",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/media/Botonchef.3f3eccd3.png"
+  },
+  {
+    "revision": "aee3089aa513804ca6694844c0de11bd",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/media/Hamburguesita.aee3089a.png"
+  },
+  {
+    "revision": "3f3eccd361c674e1cdff41193df35888",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/media/button.3f3eccd3.png"
+  },
+  {
+    "revision": "03b2e20becde27dbd5b746f361ccc294",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/media/buttonMenu.03b2e20b.png"
+  },
+  {
+    "revision": "e547d9be9e5475248e47c428b1627336",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/media/fondococina.e547d9be.png"
+  },
+  {
+    "revision": "28e7d5f31bf96044c6d3753fb44c317b",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/media/madero.28e7d5f3.png"
+  },
+  {
+    "revision": "b1e4e857dfb1049e75be658c204c4409",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/media/restaurante.b1e4e857.png"
+  },
+  {
+    "revision": "c7d0ccad233dfcf3a4b2eb9ba3ba7a43",
+    "url": "/paosarmi/BOG001-burger-queen.git/static/media/state.c7d0ccad.png"
+  }
+]);
